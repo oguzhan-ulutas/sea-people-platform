21af7027ef178c953d5d7326bc0c6f001ba9cfc7
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-06-09 19:47:53 +0300

    Add main nav

---
+++ client/src/components/MainNav.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Button } from "./ui/button";
+
+const MainNav = () => {
+  return (
+    <Button
+      variant="ghost"
+      className="font-bold hover:text-light-blue hover:bg-white"
+    >
+      Giriş Yap
+    </Button>
+  );
+};
+
+export default MainNav;
