79bdc15ceb0bb5807caaa6bfd971f0300692a300
Oguzhan Ulutas <mkoulutas@gmail.com>
2024-06-09 19:19:46 +0300

    Add mobile nav

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,9 +1,18 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import MobileNav from "./MobileNav";
 
 const Header = () => {
   return (
-    <div className="bg-cultured">
-      <h1 className="py-10 text-4xl text-light-blue">Deniz İnsanları</h1>
+    <div className="bg-cultured container mx-auto flex justify-between items-center">
+      <Link to="/">
+        <h1 className="py-10 text-3xl text-light-blue tracking-tight font-bold">
+          Deniz İnsanları
+        </h1>
+      </Link>
+      <div className="md:hidden">
+        <MobileNav />
+      </div>
     </div>
   );
 };
---
+++ client/src/components/MobileNav.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetTitle,
+  SheetTrigger,
+} from "./ui/sheet";
+import { Separator } from "./ui/separator";
+import { Button } from "./ui/button";
+import { Menu } from "lucide-react";
+
+const MobileNav = () => {
+  return (
+    <Sheet>
+      <SheetTrigger>
+        <Menu className="text-light-blue" />
+      </SheetTrigger>
+      <SheetContent className="space-y-3">
+        <SheetTitle>
+          <p className="text-center">
+            Merhaba, Denizlere sürgünlerin platformu Deniz İnsanları'na
+            hoşgeldiniz!
+          </p>
+        </SheetTitle>
+        <Separator />
+        <SheetDescription className="flex">
+          <Button className="flex-1 font-bold bg-light-blue">Log in</Button>
+        </SheetDescription>
+      </SheetContent>
+    </Sheet>
+  );
+};
+
+export default MobileNav;
